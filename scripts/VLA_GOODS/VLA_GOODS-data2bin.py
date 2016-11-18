5e2266ff53027a0dbe8e97efc93a80886e34fcc0
ebanyai <evelin.banyai@gmail.com>
2016-11-18 10:51:04 -0500

    fixed data loading

--- scripts/VLA_GOODS/VLA_GOODS-data2bin.py
+++ scripts/VLA_GOODS/VLA_GOODS-data2bin.py
@@ -7,40 +7,30 @@ Created on Thu Oct  1 12:09:37 2015
 
 import pandas as pd
 import numpy as np
-
-
-def hmsdms2dd_no(ra,dec):
-   i=0
-   newRa, newDec = np.array(np.zeros(len(ra))),np.array(np.zeros(len(dec)))
-   for item in ra:
-      newRa[i] = (int(item[0])+ int(item[1])/60.+ float(item[2])/3600.)*15
-      i+=1
-   i=0
-   for item in dec:
-      
-      if int(item[0]) < 0:
-          newDec[i] = int(item[0]) - int(item[1])/60. - float(item[2]) /3600.
-      else: 
-          newDec[i] = int(item[0]) + int(item[1])/60. + float(item[2]) /3600.
-      i+=1
-   return newRa.astype(np.float), newDec.astype(np.float)
-
+from astropy import units as u
+from astropy.coordinates import SkyCoord
 
 
 # LOAD DATA
 # source file:
-path = "C:\\Data\\ebanyai\\project\\Skyquery-data\\VLA_GOODSN\\"
-src = path+"Table_GOODSN_VLA_v1_8.txt"
+path = r"\\SKYQUERY01\Data\temp0\data0\ebanyai\VLA"
+src = path+r"\Table_GOODSN_VLA_v1_8.txt"
 cols = ["Name","RAh","RAm","RAs","e_RAs","DECd","DECm","DECs","e_DECs","SN",
         "Peak","e_Peak","Total","e_Total","bmaj","bmin","PA","ulim","beam "]
 
 
 # grab the data
-df = pd.read_table(src,names=cols,index_col=False,comment="#",skipinitialspace=True,sep=" ")
-df["RA"],df["DEC"] =  hmsdms2dd_no(df[["RAh","RAm","RAs"]].values,df[["DECd","DECm","DECs"]].values)
+df = pd.read_table(src,names=cols,index_col=False,skiprows=33,skipfooter=1,skipinitialspace=True,sep=" ")
+#df["RA"],df["DEC"] =  hmsdms2dd_no(df[["RAh","RAm","RAs"]].values,df[["DECd","DECm","DECs"]].values)
 df["objID"] = df.index+1
 
+df["ra_s"] = df["RAh"].astype(int).astype(str)+" "+df["RAm"].astype(int).astype(str)+" "+df["RAs"].astype(str)
+df["dec_s"] = df["RAh"].astype(int).astype(str)+" "+df["RAm"].astype(int).astype(str)+" "+df["RAs"].astype(str)
 
+coords = SkyCoord(df["ra_s"],df["dec_s"],unit=(u.hourangle,u.deg))
+df["RA"],df["DEC"] = coords.ra.deg, coords.dec.deg
+
+df.fillna(value=-99,inplace=True)
 # DEFINE DATA TYPES FOR BINARY FORMAT
 types = [("objID","i8"),
         ("Name","S5"),
@@ -67,7 +57,7 @@ dt_df = np.dtype(types)
 records = np.array(df.to_records(),dtype=dt_df) 
 
 # destination folder
-dst = path+"vla_goods.bin" 
+dst = path+r"\vla_goodsn.bin" 
 
 # write to file
 records.tofile(dst)
