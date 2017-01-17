42fe08b255a623b63ce0692434c7315473c252b1
Laszlo Dobos <dobos@complex.elte.hu>
2017-01-17 05:25:27 -0500

    Moved indices to appropriate file.

--- sql/GALEX/0_create.sql
+++ sql/GALEX/0_create.sql
@@ -355,49 +355,6 @@ ADD CONSTRAINT [PK_PhotoObjAll] PRIMARY KEY CLUSTERED
 
 GO
 
-CREATE NONCLUSTERED INDEX [IX_PhotoObjAll_Zone] ON [dbo].[PhotoObjAll] 
-(
-	[dec] ASC,
-	[ra] ASC,
-	[cx] ASC,
-	[cy] ASC,
-	[cz] ASC
-)WITH (DATA_COMPRESSION = PAGE, SORT_IN_TEMPDB = ON) ON [PHOTOIDX]
-GO
-
-
--- HTM index
-CREATE NONCLUSTERED INDEX [IX_PhotoObjAll_HtmID] ON [dbo].[PhotoObjAll] 
-(
-	[htmid] ASC
-)
-INCLUDE
-(
-	[ra],
-	[dec],
-	[cx],
-	[cy],
-	[cz],
-	[zoneID]
-)WITH (DATA_COMPRESSION = PAGE, SORT_IN_TEMPDB = ON) ON [PHOTOIDX]
-GO
-
--- Index to support on the fly zone table creation
-CREATE NONCLUSTERED INDEX [IX_PhotoObjAll_ZoneID] ON [dbo].[PhotoObjAll] 
-(
-	[ZoneID] ASC,
-	[ra] ASC
-)
-INCLUDE
-(
-	[dec],
-	[cx],
-	[cy],
-	[cz],
-	[htmID]
-)WITH (DATA_COMPRESSION = PAGE, SORT_IN_TEMPDB = ON) ON [PHOTOIDX]
-GO
-
 ---------------------------------------------------------------
 
 CREATE TABLE [dbo].[PhotoExtract]
