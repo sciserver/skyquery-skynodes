911f47e197be80272320645df56e3c2399beb6d1
ebanyai <evelin.banyai@gmail.com>
2017-02-21 04:37:36 -0500

    fixed metadata

--- sql/ACVS/0_create.sql
+++ sql/ACVS/0_create.sql
@@ -2,108 +2,108 @@
 
 CREATE TABLE dbo.PhotoObj 
 (
-	--/ <summary> Unique ID. </summary>	--/ <quality>meta.record</quality>
+	--/ <summary> Unique ID. </summary>	--/ <quantity>meta.record</quantity>
 	[objID] bigint NOT NULL,
 
-	--/ <summary> ASAS identification (coded from the star's RA_2000 and DEC_2000 in the format: hhmmss+ddmm.m). </summary>	--/ <quality>meta.id</quality>
+	--/ <summary> ASAS identification (coded from the star's RA_2000 and DEC_2000 in the format: hhmmss+ddmm.m). </summary>	--/ <quantity>meta.id</quantity>
 	[objName] char(13) NOT NULL,
 
-	--/ <summary> Right ascension in decimal degrees </summary>	--/ <quality>pos.eq.ra</quality>
+	--/ <summary> Right ascension in decimal degrees </summary>	--/ <quantity>pos.eq.ra</quantity>
 	--/ <unit>deg</unit>
 	[ra] float NOT NULL,
 
-	--/ <summary> Declination in decimal degrees </summary>	--/ <quality>pos.eq.dec</quality>
+	--/ <summary> Declination in decimal degrees </summary>	--/ <quantity>pos.eq.dec</quantity>
 	--/ <unit>deg</unit>
 	[dec] float NOT NULL,
 
-	--/ <summary> Cartesian X (J2000)</summary>	--/ <quality>pos.cartesian.x</quality>
+	--/ <summary> Cartesian X (J2000)</summary>	--/ <quantity>pos.cartesian.x</quantity>
 	[cx] [float] NOT NULL,
 
-	--/ <summary> Cartesian Y (J2000)</summary>	--/ <quality>pos.cartesian.y</quality>
+	--/ <summary> Cartesian Y (J2000)</summary>	--/ <quantity>pos.cartesian.y</quantity>
 	[cy] [float] NOT NULL,
 
-	--/ <summary> Cartesian Z (J2000)</summary>	--/ <quality>pos.cartesian.z</quality>
+	--/ <summary> Cartesian Z (J2000)</summary>	--/ <quantity>pos.cartesian.z</quantity>
 	[cz] [float] NOT NULL,
 
-	--/ <summary> HTM ID (J2000)</summary>	--/ <quality>pos.HTM</quality>
+	--/ <summary> HTM ID (J2000)</summary>	--/ <quantity>pos.HTM</quantity>
 	[htmid] bigint NOT NULL,
 
-	--/ <summary> Zone ID </summary>	--/ <quality>pos.zone</quality>
+	--/ <summary> Zone ID </summary>	--/ <quantity>pos.zone</quantity>
 	[zoneid] int NOT NULL,
 
-	--/ <summary> Period in days (or characteristic time scale of variation for irregular objects). </summary>	--/ <quality>time.period; src.var</quality>
+	--/ <summary> Period in days (or characteristic time scale of variation for irregular objects). </summary>	--/ <quantity>time.period; src.var</quantity>
 	--/ <unit> d </unit>
 	[PER]  real NOT NULL,
 
-	--/ <summary> Epoch of minimum (for eclipsing) or maximum (for pulsating) brightness. </summary>	--/ <quality>time.epoch</quality>
+	--/ <summary> Epoch of minimum (for eclipsing) or maximum (for pulsating) brightness. </summary>	--/ <quantity>time.epoch</quantity>
 	[HJD0] float NOT NULL,
 
-	--/ <summary> Brightness at maximum. </summary>	--/ <quality>phot.mag; stat.max</quality>
+	--/ <summary> Brightness at maximum. </summary>	--/ <quantity>phot.mag; stat.max</quantity>
 	--/ <unit> mag </unit>
 	[VMAX] real NOT NULL,
 
-	--/ <summary> Amplitude of variation. </summary>	--/ <quality>src.var.amplitude</quality>
+	--/ <summary> Amplitude of variation. </summary>	--/ <quantity>src.var.amplitude</quantity>
 	--/ <unit> mag </unit>
 	[VAMP] real NOT NULL,
 
-	--/ <summary> One of the predefined classes: DSCT, RRC, RRAB, DCEP_FU, DCEP_FO, CW, ACV, BCEP, MIRA and MISC, or its combination. </summary>	--/ <quality>src.var.type</quality>
+	--/ <summary> One of the predefined classes: DSCT, RRC, RRAB, DCEP_FU, DCEP_FO, CW, ACV, BCEP, MIRA and MISC, or its combination. </summary>	--/ <quantity>src.var.type</quantity>
 	[TYPE] char(27) NOT NULL,
 
-	--/ <summary> GCVS name. </summary>	--/ <quality>meta.id.cross</quality>
+	--/ <summary> GCVS name. </summary>	--/ <quantity>meta.id.cross</quantity>
 	[GCVS_ID] char(19) NOT NULL,
 
-	--/ <summary> GCVS variability type. </summary>	--/ <quality>src.var.type</quality>
+	--/ <summary> GCVS variability type. </summary>	--/ <quantity>src.var.type</quantity>
 	[GCVS_TYPE] char(13) NOT NULL,
 
-	--/ <summary> IRAS flux at 12 microns (listed as -2.5 log flux). </summary>	--/ <quality>phot.flux; em.IR.IRAS.12</quality>
+	--/ <summary> IRAS flux at 12 microns (listed as -2.5 log flux). </summary>	--/ <quantity>phot.flux; em.IR.IRAS.12</quantity>
 	--/ <unit> Jy </unit>
 	[IR12]  real NOT NULL,
 
-	--/ <summary>  IRAS flux at 25 microns. </summary>	--/ <quality>phot.flux; em.IR.IRAS.25</quality>
+	--/ <summary>  IRAS flux at 25 microns. </summary>	--/ <quantity>phot.flux; em.IR.IRAS.25</quantity>
 	--/ <unit> Jy </unit>
 	[IR25] real NOT NULL,
 
-	--/ <summary>  IRAS flux at 60 microns. </summary>	--/ <quality>phot.flux; em.IR.IRAS.60</quality>
+	--/ <summary>  IRAS flux at 60 microns. </summary>	--/ <quantity>phot.flux; em.IR.IRAS.60</quantity>
 	--/ <unit> Jy </unit>
 	[IR60] real NOT NULL,
 
-	--/ <summary>  IRAS flux at 100 microns. </summary>	--/ <quality>phot.flux; em.IR.IRAS.100</quality>
+	--/ <summary>  IRAS flux at 100 microns. </summary>	--/ <quantity>phot.flux; em.IR.IRAS.100</quantity>
 	--/ <unit> Jy </unit>
 	[IR100] real NOT NULL,
 
-	--/ <summary> 2MASS J photometry. </summary>	--/ <quality>phot.mag; em.IR.J</quality>
+	--/ <summary> 2MASS J photometry. </summary>	--/ <quantity>phot.mag; em.IR.J</quantity>
 	--/ <unit> mag </unit>
 	[J] real NOT NULL,
 
-	--/ <summary> 2MASS H photometry. </summary>	--/ <quality>phot.mag; em.IR.H</quality>
+	--/ <summary> 2MASS H photometry. </summary>	--/ <quantity>phot.mag; em.IR.H</quantity>
 	--/ <unit> mag </unit>
 	[H] real NOT NULL,
 
-	--/ <summary> 2MASS K photometry. </summary>	--/ <quality>phot.mag; em.IR.K</quality>
+	--/ <summary> 2MASS K photometry. </summary>	--/ <quantity>phot.mag; em.IR.K</quantity>
 	--/ <unit> mag </unit>
 	[K]  real NOT NULL,
 
-	--/ <summary> V-IR12 color index. </summary>	--/ <quality>phot.color; em.opt.V; em.IR.IRAS.12</quality>
+	--/ <summary> V-IR12 color index. </summary>	--/ <quantity>phot.color; em.opt.V; em.IR.IRAS.12</quantity>
 	--/ <unit> mag </unit>
 	[V_IR12] real NOT NULL,
 
-	--/ <summary> V-J color index. </summary>	--/ <quality>phot.color; em.opt.V; em.IR.J</quality>
+	--/ <summary> V-J color index. </summary>	--/ <quantity>phot.color; em.opt.V; em.IR.J</quantity>
 	--/ <unit> mag </unit>
 	[V_J] real NOT NULL,
 
-	--/ <summary> V-H color index. </summary>	--/ <quality>phot.color; em.opt.V; em.IR.H</quality>
+	--/ <summary> V-H color index. </summary>	--/ <quantity>phot.color; em.opt.V; em.IR.H</quantity>
 	--/ <unit> mag </unit>
 	[V_H] real NOT NULL,
 
-	--/ <summary> V-K color index. </summary>	--/ <quality>phot.color; em.opt.V; em.IR.K</quality>
+	--/ <summary> V-K color index. </summary>	--/ <quantity>phot.color; em.opt.V; em.IR.K</quantity>
 	--/ <unit> mag </unit>
 	[V_K] real NOT NULL,
 
-	--/ <summary> J-H color index. </summary>	--/ <quality>phot.color; em.IR.J; em.IR.H</quality>
+	--/ <summary> J-H color index. </summary>	--/ <quantity>phot.color; em.IR.J; em.IR.H</quantity>
 	--/ <unit> mag </unit>
 	[J_H] real NOT NULL,
 
-	--/ <summary> H-K color index. </summary>	--/ <quality>phot.color; em.IR.H; em.IR.K</quality>
+	--/ <summary> H-K color index. </summary>	--/ <quantity>phot.color; em.IR.H; em.IR.K</quantity>
 	--/ <unit> mag </unit>
 	[H_K]  real NOT NULL,
 
