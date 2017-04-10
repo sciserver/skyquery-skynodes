a0526a8990066d359e1d7184a1231067849ad8dc
ebanyai <evelin.banyai@gmail.com>
2017-04-10 04:05:09 -0400

    added columns (record id, cx, cz, cy, htmid, zoneid)

--- sql/GAIADR1/0_create.sql
+++ sql/GAIADR1/0_create.sql
@@ -1,60 +1,259 @@
 CREATE TABLE Source
 (
-	solution_id bigint NOT NULL,
-	source_id bigint NOT NULL,
-	random_index bigint NOT NULL,
-	ref_epoch float NOT NULL,
-	ra float NOT NULL,
-	ra_error float NOT NULL,
-	dec float NOT NULL,
-	dec_error float NOT NULL,
-	parallax float NULL,
-	parallax_error float NULL,
-	pmra float NULL,
-	pmra_error real NULL,
-	pmdec float NULL,
-	pmdec_error float NULL,
-	ra_dec_corr real NOT NULL,
-	ra_parallax_corr real NULL,
-	ra_pmra_corr real NULL,
-	ra_pmdec_corr real NULL,
-	dec_parallax_corr real NULL,
-	dec_pmra_corr real NULL,
-	dec_pmdec_corr real NULL,
-	parallax_pmra_corr real NULL,
-	parallax_pmdec_corr real NULL,
-	pmra_pmdec_corr real NULL,
-	astrometric_n_obs_al int NOT NULL,
-	astrometric_n_obs_ac int NOT NULL,
-	astrometric_n_good_obs_al int NOT NULL,
-	astrometric_n_good_obs_ac int NOT NULL,
-	astrometric_n_bad_obs_al int NOT NULL,
-	astrometric_n_bad_obs_ac int NOT NULL,
-	astrometric_delta_q float NULL,
-	astrometric_excess_noise float NOT NULL,
-	astrometric_excess_noise_sig float NOT NULL,
-	astrometric_primary_flag varchar(5) NOT NULL,	-- ********
-	astrometric_relegation_factor real NOT NULL,
-	astrometric_weight_al real NULL,
-	astrometric_weight_ac real NULL,
-	astrometric_priors_used int NOT NULL,
-	matched_observations smallint NOT NULL,
-	duplicated_source varchar(5) NOT NULL,			-- ********
-	scan_direction_strength_k1 real NOT NULL,
-	scan_direction_strength_k2 real NOT NULL,
-	scan_direction_strength_k3 real NOT NULL,
-	scan_direction_strength_k4 real NOT NULL,
-	scan_direction_mean_k1 real NOT NULL,
-	scan_direction_mean_k2 real NOT NULL,
-	scan_direction_mean_k3 real NOT NULL,
-	scan_direction_mean_k4 real NOT NULL,
-	phot_g_n_obs int NOT NULL,
-	phot_g_mean_flux float NOT NULL,
-	phot_g_mean_flux_error float NOT NULL,
-	phot_g_mean_mag float NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[record_id] [bigint] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[solution_id] [bigint] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[source_id] [bigint] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[random_index] [bigint] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[ref_epoch] [float] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[ra] [float] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[ra_error] [float] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[dec] [float] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[dec_error] [float] NOT NULL,
+	
+	--/ <summary> Cartesian X (J2000)</summary>
+	--/ <quantity>pos.eq.x</quantity>
+	[cx] [float] NOT NULL,
+
+	--/ <summary> Cartesian Y (J2000)</summary>
+	--/ <quantity>pos.eq.y</quantity>
+	[cy] [float] NOT NULL,
+
+	--/ <summary> Cartesian Z (J2000)</summary>
+	--/ <quantity>pos.eq.z</quantity>
+	[cz] [float] NOT NULL,
+
+	--/ <summary> HTM ID (J2000)</summary>
+	--/ <quantity>pos.HTM</quantity>
+	[htmid] bigint NOT NULL,
+
+	--/ <summary> Zone ID </summary>
+	--/ <quantity>pos.zone</quantity>
+	[zoneid] int NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[parallax] [float] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[parallax_error] [float] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[pmra] [float] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[pmra_error] [real] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[pmdec] [float] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[pmdec_error] [float] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[ra_dec_corr] [real] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[ra_parallax_corr] [real] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[ra_pmra_corr] [real] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[ra_pmdec_corr] [real] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[dec_parallax_corr] [real] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[dec_pmra_corr] [real] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[dec_pmdec_corr] [real] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[parallax_pmra_corr] [real] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[parallax_pmdec_corr] [real] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[pmra_pmdec_corr] [real] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[astrometric_n_obs_al] [int] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[astrometric_n_obs_ac] [int] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[astrometric_n_good_obs_al] [int] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[astrometric_n_good_obs_ac] [int] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[astrometric_n_bad_obs_al] [int] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[astrometric_n_bad_obs_ac] [int] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[astrometric_delta_q] [float] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[astrometric_excess_noise] [float] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[astrometric_excess_noise_sig] [float] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[astrometric_primary_flag] [bit] NOT NULL,	-- ********
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[astrometric_relegation_factor] [real] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[astrometric_weight_al] [real] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[astrometric_weight_ac] [real] NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[astrometric_priors_used] [int] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[matched_observations] [smallint] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[duplicated_source] [bit] NOT NULL,	-- ********		
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[scan_direction_strength_k1] [real] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[scan_direction_strength_k2] [real] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[scan_direction_strength_k3] [real] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[scan_direction_strength_k4] [real] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[scan_direction_mean_k1] [real] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[scan_direction_mean_k2] [real] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[scan_direction_mean_k3] [real] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[scan_direction_mean_k4] [real] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[phot_g_n_obs] [int] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[phot_g_mean_flux] [float] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[phot_g_mean_flux_error] [float] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[phot_g_mean_mag] [float] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
 	phot_variable_flag varchar(50) NULL,
-	l float NOT NULL,
-	b float NOT NULL,
-	ecl_lon float NOT NULL,
-	ecl_lat float NOT NULL
-)
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[l] [float] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[b] [float] NOT NULL,
+	
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[ecl_lon] [float] NOT NULL,
+
+	--/ <summary></summary>
+	--/ <quantity></quantity>
+	[ecl_lat] [float NOT NULL
+	CONSTRAINT [PKSource] PRIMARY KEY CLUSTERED 
+(
+	[record_id] ASC
+)WITH (DATA_COMPRESSION = PAGE, PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [SOURCES]
